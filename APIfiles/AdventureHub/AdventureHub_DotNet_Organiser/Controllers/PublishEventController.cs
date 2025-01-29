021ee21c4d37b5b3302d90a1883869ab64ae1cab
Mangesh Dete <dete.mangesh2424@gmail.com>
2025-01-29 11:42:10 +0530

    File Names Changed and DOTNET API files added

---
+++ APIfiles/AdventureHub/AdventureHub_DotNet_Organiser/Controllers/PublishEventController.cs
@@ -0,0 +1,26 @@
+﻿using AdventureHub.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdventureHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PublishEventController : Controller
+    {
+        public static MyDbContext Db { get; }
+
+        static PublishEventController()
+        {
+            Db = new MyDbContext();
+        }
+
+        //Get All the Events which are published by any Organiser by Organiser Id
+        [HttpGet]
+        public IActionResult GetPublishedEventsByOrganiserId([FromQuery] int orgId)
+        {
+            var publishedEvents=Db.Publishevents.Include(p => p.City).Include(p => p.Event).Where(e => e.Organiserid == orgId).ToList();
+            return Ok(publishedEvents);
+        }
+    }
+}
---
+++ APIfiles/AdventureHub/AdventureHub_DotNet_Organiser/Models/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventureHub.Models
+{
+    public partial class State
+    {
+        public State()
+        {
+            Cities = new HashSet<City>();
+        }
+
+        public int Stateid { get; set; }
+        public string Statename { get; set; } = null!;
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
