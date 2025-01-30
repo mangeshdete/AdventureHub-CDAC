e4cba2af7c762a2b0535c20bbaba992143d1f84d
Mangesh Dete <dete.mangesh2424@gmail.com>
2025-01-31 00:12:13 +0530

    Normal updates

--- APIfiles/AdventureHub/AdventureHub_DotNet_Organiser/obj/Debug/net6.0/AdventureHub.AssemblyInfo.cs
+++ APIfiles/AdventureHub/AdventureHub_DotNet_Organiser/obj/Debug/net6.0/AdventureHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventureHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592e981a7bbd23ec7fffa00fc7550c00da2e496d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a115e9290f1008f231bafcf9a586b462c0c3e6b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventureHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventureHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
