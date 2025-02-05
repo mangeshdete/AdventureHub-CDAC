939f44135ab705e3c311bf31b7b3f6156f8bdd3b
Mangesh Dete <dete.mangesh2424@gmail.com>
2025-02-05 18:05:04 +0530

    Refunds Integration Done for both Customer and Organiser

--- APIfiles/AdventureHub/AdventureHub_DotNet_Customer/obj/Debug/net6.0/AdventureHub_DotNet_Customer.AssemblyInfo.cs
+++ APIfiles/AdventureHub/AdventureHub_DotNet_Customer/obj/Debug/net6.0/AdventureHub_DotNet_Customer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventureHub_DotNet_Customer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdd3ebc97ebf4ea86e2b0cded9d77c65b2709bb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4974a48cf50551ba2cbc8afe3acf2284828f1284")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventureHub_DotNet_Customer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventureHub_DotNet_Customer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
